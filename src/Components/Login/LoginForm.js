013a387451229973df06bed62c1a8f8e3dcd7df1
JessianCRB <jessian.ribeiro@hotmail.com>
2022-02-14 09:05:51 -0300

    login route e Hook useForm

---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+
+const LoginForm = () => {
+  const username = useForm('email');
+  const password = useForm();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Login realizado! ');
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastros</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
