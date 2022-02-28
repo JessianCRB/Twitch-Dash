9695b2e2c1de582c716689eff7dd7fafe9696530
JessianCRB <jessian.ribeiro@hotmail.com>
2022-02-28 11:34:56 -0300

    Layout-Streamers

--- src/Components/Streamers.js
+++ src/Components/Streamers.js
@@ -5,7 +5,10 @@ import { ReactComponent as Mao } from '../Assets/mão.svg';
 
 const Streamers = () => {
   return (
-    <div className="container" style={{ paddingTop: '95px' }}>
+    <div
+      className="container"
+      style={{ paddingTop: '95px', paddingBottom: '95px' }}
+    >
       <div className={styles.streamersGrid}>
         <div className={styles.cardsStream}>
           <div className={styles.avatar}>
@@ -43,21 +46,275 @@ const Streamers = () => {
         </div>
 
         <div className={styles.cardsStream}>
-          <div className={styles.avatar}>avatar aqui 2</div>
-          <div className={styles.name}>nome aqui 2</div>
-          <div className={styles.info}>informações aqui 2</div>
+          <div className={styles.avatar}>
+            <img
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/1cce6c41-aaf8-44c2-ad1b-ef76fb0eede3-profile_image-300x300.png"
+              alt="Yoda"
+            />
+          </div>
+          <a
+            className={styles.name}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitch.tv/yoda?lang=pt-br"
+          >
+            Yoda
+          </a>
+          <div className={styles.info}>
+            <svg className={`${styles.svg1} ${styles.svg2}`} role="img">
+              <Coracao />
+            </svg>
+            <div style={{ textAlign: 'center', marginRight: '12px' }}>
+              2,25M
+            </div>
+            <div className={styles.info2}>
+              <svg
+                className={`${styles.svg1} ${styles.svg2}`}
+                style={{ maxWidth: '14px' }}
+                role="img"
+              >
+                <Mao />
+              </svg>
+              <div style={{ textAlign: 'center' }}>Partner</div>
+            </div>
+          </div>
         </div>
 
         <div className={styles.cardsStream}>
-          <div className={styles.avatar}>avatar aqui 3</div>
-          <div className={styles.name}>nome aqui 3</div>
-          <div className={styles.info}>informações aqui 3</div>
+          <div className={styles.avatar}>
+            <img
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/f4b12683-57ff-4b57-926a-67512b43a7ff-profile_image-300x300.png"
+              alt="Gaules"
+            />
+          </div>
+          <a
+            className={styles.name}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitch.tv/gaules?lang=pt-br"
+          >
+            Gaules
+          </a>
+          <div className={styles.info}>
+            <svg className={`${styles.svg1} ${styles.svg2}`} role="img">
+              <Coracao />
+            </svg>
+            <div style={{ textAlign: 'center', marginRight: '12px' }}>3,3M</div>
+            <div className={styles.info2}>
+              <svg
+                className={`${styles.svg1} ${styles.svg2}`}
+                style={{ maxWidth: '14px' }}
+                role="img"
+              >
+                <Mao />
+              </svg>
+              <div style={{ textAlign: 'center' }}>Partner</div>
+            </div>
+          </div>
         </div>
 
         <div className={styles.cardsStream}>
-          <div className={styles.avatar}>avatar aqui 4</div>
-          <div className={styles.name}>nome aqui 4</div>
-          <div className={styles.info}>informações aqui 4</div>
+          <div className={styles.avatar}>
+            <img
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/32805a78-d927-48bd-8089-bf5efed53ea4-profile_image-300x300.png"
+              alt="Casimiro"
+            />
+          </div>
+          <a
+            className={styles.name}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitch.tv/casimito?lang=pt-br"
+          >
+            Casimito
+          </a>
+          <div className={styles.info}>
+            <svg className={`${styles.svg1} ${styles.svg2}`} role="img">
+              <Coracao />
+            </svg>
+            <div style={{ textAlign: 'center', marginRight: '12px' }}>
+              1,97M
+            </div>
+            <div className={styles.info2}>
+              <svg
+                className={`${styles.svg1} ${styles.svg2}`}
+                style={{ maxWidth: '14px' }}
+                role="img"
+              >
+                <Mao />
+              </svg>
+              <div style={{ textAlign: 'center' }}>Partner</div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardsStream}>
+          <div className={styles.avatar}>
+            <img
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/da80c83c-88f3-4c02-8768-b38ee70882f4-profile_image-300x300.png"
+              alt="JuMayumin"
+            />
+          </div>
+          <a
+            className={styles.name}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitch.tv/jumayumin?lang=pt-br"
+          >
+            JuMayumin
+          </a>
+          <div className={styles.info}>
+            <svg className={`${styles.svg1} ${styles.svg2}`} role="img">
+              <Coracao />
+            </svg>
+            <div style={{ textAlign: 'center', marginRight: '12px' }}>
+              436,9k
+            </div>
+            <div className={styles.info2}>
+              <svg
+                className={`${styles.svg1} ${styles.svg2}`}
+                style={{ maxWidth: '14px' }}
+                role="img"
+              >
+                <Mao />
+              </svg>
+              <div style={{ textAlign: 'center' }}>Partner</div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardsStream}>
+          <div className={styles.avatar}>
+            <img
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/15cec952-c1ba-4ff8-a79c-53c2fa5bd269-profile_image-300x300.png"
+              alt="Alanzoka"
+            />
+          </div>
+          <a
+            className={styles.name}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitch.tv/alanzoka?lang=pt-br"
+          >
+            Alanzoka
+          </a>
+          <div className={styles.info}>
+            <svg className={`${styles.svg1} ${styles.svg2}`} role="img">
+              <Coracao />
+            </svg>
+            <div style={{ textAlign: 'center', marginRight: '12px' }}>
+              5,55M
+            </div>
+            <div className={styles.info2}>
+              <svg
+                className={`${styles.svg1} ${styles.svg2}`}
+                style={{ maxWidth: '14px' }}
+                role="img"
+              >
+                <Mao />
+              </svg>
+              <div style={{ textAlign: 'center' }}>Partner</div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardsStream}>
+          <div className={styles.avatar}>
+            <img
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/762a31e8-9829-4957-8073-dd85d67ad576-profile_image-300x300.png"
+              alt="Bagi"
+            />
+          </div>
+          <a
+            className={styles.name}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitch.tv/bagi?lang=pt"
+          >
+            Bagi
+          </a>
+          <div className={styles.info}>
+            <svg className={`${styles.svg1} ${styles.svg2}`} role="img">
+              <Coracao />
+            </svg>
+            <div style={{ textAlign: 'center', marginRight: '12px' }}>627k</div>
+            <div className={styles.info2}>
+              <svg
+                className={`${styles.svg1} ${styles.svg2}`}
+                style={{ maxWidth: '14px' }}
+                role="img"
+              >
+                <Mao />
+              </svg>
+              <div style={{ textAlign: 'center' }}>Partner</div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardsStream}>
+          <div className={styles.avatar}>
+            <img
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/4c8846c2-2fad-4276-8004-070700d316aa-profile_image-300x300.png"
+              alt="Maethe"
+            />
+          </div>
+          <a
+            className={styles.name}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitch.tv/maethe?lang=pt-br"
+          >
+            Maethe
+          </a>
+          <div className={styles.info}>
+            <svg className={`${styles.svg1} ${styles.svg2}`} role="img">
+              <Coracao />
+            </svg>
+            <div style={{ textAlign: 'center', marginRight: '12px' }}>401k</div>
+            <div className={styles.info2}>
+              <svg
+                className={`${styles.svg1} ${styles.svg2}`}
+                style={{ maxWidth: '14px' }}
+                role="img"
+              >
+                <Mao />
+              </svg>
+              <div style={{ textAlign: 'center' }}>Partner</div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardsStream}>
+          <div className={styles.avatar}>
+            <img
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/371a4ddd-c4ef-418c-9f42-cfecda78187d-profile_image-300x300.png"
+              alt="Loud Coringa"
+            />
+          </div>
+          <a
+            className={styles.name}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitch.tv/loud_coringa?lang=pt-br"
+          >
+            Loud_Coringa
+          </a>
+          <div className={styles.info}>
+            <svg className={`${styles.svg1} ${styles.svg2}`} role="img">
+              <Coracao />
+            </svg>
+            <div style={{ textAlign: 'center', marginRight: '12px' }}>2,9M</div>
+            <div className={styles.info2}>
+              <svg
+                className={`${styles.svg1} ${styles.svg2}`}
+                style={{ maxWidth: '14px' }}
+                role="img"
+              >
+                <Mao />
+              </svg>
+              <div style={{ textAlign: 'center' }}>Partner</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
